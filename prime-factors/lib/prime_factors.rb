04e746b1582b57f46433f5ac4ed506b9127579d4
Matt Wynne <matt@mattwynne.net>
2009-01-04 23:35:35 +0000

    * Factored out algorithm into a module   * Refactored specs to generate the examples from a hash

--- prime-factors/lib/prime_factors.rb
+++ prime-factors/lib/prime_factors.rb
@@ -1,14 +1,20 @@
 class PrimeFactors
-  def self.generate(n)
-    primes = []
-    candidate = 2
-    while n > 1
-      while n % candidate == 0
-        primes << candidate
-        n /= candidate
+  
+  module Generator
+    def generate(n)
+      primes = []
+      candidate = 2
+      while n > 1
+        while n % candidate == 0
+          primes << candidate
+          n /= candidate
+        end
+        candidate += 1
       end
-      candidate += 1
+      primes
     end
-    primes
   end
+
+  extend Generator
+  
 end
--- prime-factors/spec/prime_factors_spec.rb
+++ prime-factors/spec/prime_factors_spec.rb
@@ -3,33 +3,25 @@ require 'rubygems'
 require 'spec'
 
 describe PrimeFactors, "#generate" do
-
-  it "should return an empty array for 1" do
-    PrimeFactors.generate(1).should == []
-  end
-
-  it "should return [2] for 2" do
-    PrimeFactors.generate(2).should == [2]
-  end
-
-  it "should return [3] for 3" do
-    PrimeFactors.generate(3).should == [3]
-  end
-
-  it "should return [2,2] for 4" do
-    PrimeFactors.generate(4).should == [2,2]
-  end
-
-  it "should return [2,3] for 6" do
-    PrimeFactors.generate(6).should == [2,3]
-  end
-
-  it "should return [2,2,2] for 8" do
-    PrimeFactors.generate(8).should == [2,2,2]
-  end
-
-  it "should return [3,3] for 9" do
-    PrimeFactors.generate(9).should == [3,3]
+  
+  include PrimeFactors::Generator
+  
+  examples = {
+    1 => [],
+    2 => [2],
+    3 => [3],
+    4 => [2, 2],
+    6 => [2, 3],
+    8 => [2, 2, 2],
+    9 => [3, 3],
+  }
+  
+  examples.each do |number, expected_result|
+    
+    it "given #{number}, it should return [#{expected_result.to_s}]" do
+      generate(number).should == expected_result
+    end
+    
   end
 
 end
