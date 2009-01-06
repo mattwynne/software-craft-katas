b65d5e2ba6e6236c69ef0e8abeab2ccf2547665d
Matt Wynne <matt@mattwynne.net>
2009-01-06 18:28:52 +0000

    * Putting solution back

--- minesweeper/lib/game.rb
+++ minesweeper/lib/game.rb
@@ -11,14 +11,51 @@ end
 
 class GameBoard
   def initialize(width, height)
-    #TODO
+    @width, @height = width, height
+    initialize_board
   end
 
+  def initialize_board
+    @board = Array.new(@height) do 
+       Array.new(@width, 0) 
+    end
+  end
+  
   def reveal_on(output)
-    #TODO
+    result = ""
+    
+    @board.each do |row|
+      row.each do |cell|
+        result += display_for_cell(cell)
+      end
+      result += "\n"
+    end
+    
+    output.puts(result[0..-2])
+  end
+  
+  def display_for_cell(count)
+    if count == 0
+      '.'
+    elsif count == -1
+      '*'
+    else
+      count.to_s
+    end
   end
 
   def place_mine(x, y)
-    #TODO
+    for i in (-1..1) 
+      for j in (-1..1)
+        x_plus_i = x + i
+        y_plus_j = y + j
+        if x_plus_i >= 0 and x_plus_i < @width and y_plus_j >= 0 and y_plus_j < @height and @board[y_plus_j][x_plus_i] != -1
+          @board[y_plus_j][x_plus_i] += 1
+        end
+      end
+    end
+    @board[y][x] = -1    
   end
 end
+
+ 
